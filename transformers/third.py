0545000fa86523e61efe9b9f085735d4402e29c1
navdeep710 <navdeep710@gmail.com>
2024-08-14 07:44:30 +0530

    added new fields

--- transformers/third.py
+++ transformers/third.py
@@ -1,7 +1,13 @@
 from funcy import project
 
 from utils import elasticsearch_utils as es_utils
+from utils.dictionary_utils import safe_get_in, safe_int_parse
 
+
+def populate_in_stock(item):
+    # Item.quantity.count
+    quantity = safe_int_parse(safe_get_in(item, ["item_details","quantity","available", "count"], 0))
+    return quantity > 0
 
 def update_provider_items_with_manual_flags(provider_id, provider_items):
     manually_flagged_items = es_utils.get_all_manually_flagged_items_for_provider(provider_id)
@@ -19,4 +25,5 @@ def update_provider_items_with_manual_flags(provider_id, provider_items):
             else item.get("auto_seller_flag", False)
         item["provider_flag"] = item["manual_provider_flag"] if item.get("manual_provider_flag") is not None \
             else item.get("auto_provider_flag", False)
+        item['in_stock'] = populate_in_stock(item)
     return provider_items

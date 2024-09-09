572f2f73bdef104029a7e3c9595a3947954e7e8a
Aditya Patil <ad1tya.patil.3297@gmail.com>
2024-09-09 12:33:32 +0530

    Add log error for bhashini api failure

--- consumers/translator.py
+++ consumers/translator.py
@@ -5,6 +5,7 @@ from retry import retry
 
 from config import get_config_by_name
 from event_producer import publish_message
+from logger.custom_logging import log_error
 from transformers.translation import translate_items_into_target_language, translate_locations_into_target_language
 from utils.redis_utils import init_redis_cache
 from utils.rabbitmq_utils import declare_queue, consume_message, open_connection_and_channel_if_not_already_open, \
@@ -18,14 +19,17 @@ def consume_fn(message_string):
     rabbitmq_channel = create_channel(rabbitmq_connection)
     declare_queue(rabbitmq_channel, es_dumper_queue)
 
-    if message["index"] == "items":
-        translated_items = translate_items_into_target_language(message["data"], message["lang"])
-        publish_message(rabbitmq_channel, es_dumper_queue, {"index": "items", "data": translated_items})
-    elif message["index"] == "locations":
-        translated_locations = translate_locations_into_target_language(message["data"], message["lang"])
-        publish_message(rabbitmq_channel, es_dumper_queue, {"index": "locations", "data": translated_locations})
-
-    close_channel_and_connection(rabbitmq_channel, rabbitmq_connection)
+    try:
+        if message["index"] == "items":
+            translated_items = translate_items_into_target_language(message["data"], message["lang"])
+            publish_message(rabbitmq_channel, es_dumper_queue, {"index": "items", "data": translated_items})
+        elif message["index"] == "locations":
+            translated_locations = translate_locations_into_target_language(message["data"], message["lang"])
+            publish_message(rabbitmq_channel, es_dumper_queue, {"index": "locations", "data": translated_locations})
+    except Exception as e:
+        log_error(f"Got an exception while translating {e}")
+    finally:
+        close_channel_and_connection(rabbitmq_channel, rabbitmq_connection)
 
 
 @retry(AMQPConnectionError, delay=5, jitter=(1, 3))
